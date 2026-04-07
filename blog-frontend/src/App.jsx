71ee3e77a5adb43054d3621580867db5dc4066ee
dayeon1222 <ekdus1222@naver.com>
2026-04-07 16:54:59 +0900

    fix: 배포오류 확인 콘솔

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -32,7 +32,7 @@ const LoadingFallback = styled.div`
 
 const App = () => {
   useEffect(() => {
-    console.log(import.meta.env.VITE_API_URL);
+    console.log(버전2);
   }, []);
   return (
     <>
