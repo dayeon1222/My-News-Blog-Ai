27bf2c374a63a395abafa2e1c0d16f4271c4d25c
dayeon1222 <ekdus1222@naver.com>
2026-04-07 19:57:05 +0900

    fix: 프론트엔드 mainpage apit 주소 변경

--- blog-frontend/src/pages/Mainpage.jsx
+++ blog-frontend/src/pages/Mainpage.jsx
@@ -190,7 +190,7 @@ const MainPage = () => {
   } = useQuery({
     queryKey: ['latestNews'],
     queryFn: async () => {
-      const { data } = await client.get(`${API}/api/news/latest`);
+      const { data } = await API.get('/api/news/latest');
       return data;
     },
     staleTime: 1000 * 60 * 5,
