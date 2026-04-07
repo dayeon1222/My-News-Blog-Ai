dd59033aeec030b61e585be038cb53f0e28df3ea
dayeon1222 <ekdus1222@naver.com>
2026-04-07 17:52:17 +0900

    fix: 메인페이지  경로수정

--- blog-frontend/src/pages/Mainpage.jsx
+++ blog-frontend/src/pages/Mainpage.jsx
@@ -3,7 +3,7 @@ import styled, { keyframes } from 'styled-components';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import API from '../../src/api/client';
+import API from '../api/client';
 
 // --- Animations ---
 
